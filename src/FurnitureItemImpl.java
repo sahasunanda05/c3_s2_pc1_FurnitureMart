d25f325ba20f5b626ce39a9a931b5efbff5b18e9
SUNANDA SAHA <sahasunanda73@gmail.com>
2022-08-11 15:55:56 +0530

    scanner added

--- src/FurnitureItemImpl.java
+++ src/FurnitureItemImpl.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class FurnitureItemImpl {
     public static void main(String[] args) {
         FurnitureItem furnitureItem = new FurnitureItem();
+
+        Scanner scanner = new Scanner(System.in);
 
         furnitureItem.furnitureCode = 0;
         furnitureItem.furnitureType = "stools";
@@ -9,15 +12,29 @@ public class FurnitureItemImpl {
         furnitureItem.gradeOfFurniture = "grade 1";
         furnitureItem.price = 800;
 
-        System.out.println("furniture code = " + furnitureItem.furnitureCode);
-        System.out.println("furniture type = " + furnitureItem.furnitureType);
-        System.out.println("furniture usage = " + furnitureItem.furnitureUsage);
-        System.out.println("color = " + furnitureItem.color);
-        System.out.println("grade of furniture = " + furnitureItem.gradeOfFurniture);
-        System.out.println("price = " + furnitureItem.price);
+        System.out.println("Enter furniture code (0/1/2/3) : ");
+        furnitureItem.furnitureCode = scanner.nextInt();
+
+        System.out.println("Enter furniture type (stool/chair/cupboard/table) : ");
+        scanner.nextLine();
+        furnitureItem.furnitureType = scanner.nextLine();
+
+        System.out.println("Enter furniture type (outdoor/indoor) : ");
+        furnitureItem.furnitureUsage = scanner.nextLine();
+
+        System.out.println("Enter furniture color (red/blue/green) : ");
+        furnitureItem.color = scanner.nextLine();
+
+
+        System.out.println("Enter the grade of furniture (1/2/3) ");
+        furnitureItem.gradeOfFurniture = scanner.nextLine();
+
+        System.out.println("Enter the price : ");
+        furnitureItem.price = scanner.nextDouble();
+
 
         System.out.println("Discounted Price:" + furnitureItem.getFinalPriceAfterDiscount());
-        //commment
+
 
     }
 }
